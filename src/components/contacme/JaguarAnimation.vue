82f092ecbd09313b6de47d25793a903de1ce0235
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2021-12-17 17:22:53 -0300

    add Jaguar animation, vueUse

---
+++ src/components/contacme/JaguarAnimation.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="position-jaguar" v-if="isLargeScreen">
+    <div class="jaguarSprites"></div>
+  </div>
+</template>
+<script>
+import { gsap } from "gsap";
+import { useMediaQuery } from "@vueuse/core";
+import { onMounted } from "vue";
+
+export default {
+  setup() {
+    const isLargeScreen = useMediaQuery("(min-width: 780px)");
+    if (isLargeScreen) {
+      setTimeout(() => {
+           let dragon = gsap.timeline({ repeat: -1 });
+          dragon.to(".position-jaguar", {
+            duration: 4,
+            x: "110vw",
+            y: 0,
+            ease: "sine.in",
+            delay: 2,
+          });
+      }, 500);
+    }
+    return { isLargeScreen };
+  },
+  mounted() {},
+};
+</script>
+<style lang="scss" scoped>
+.jaguarSprites {
+  width: 165px;
+  height: 200px;
+  position: relative;
+  left: -170px;
+  background-image: url("https://assets.codepen.io/4698468/jaguar.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  animation: move 0.8s steps(12) infinite;
+}
+@keyframes move {
+  from {
+    background-position: 4% center;
+  }
+  to {
+    background-position-x: -2230px;
+  }
+}
+.position-jaguar {
+  position: fixed;
+  bottom: -80px;
+  left: 0;
+}
+</style>
--- src/views/Contacme.vue
+++ src/views/Contacme.vue
@@ -6,48 +6,19 @@
     <section class="section2">
       <ContactForm />
     </section>
+  <JaguarAnimation />
   </div>
 </template>
 <script>
 import ContactForm from "../components/contacme/ContacForm.vue";
 import CloudTag from "../components/contacme/CloudTag.vue";
+import JaguarAnimation from "../components/contacme/JaguarAnimation.vue";
 
 export default {
   components: {
     ContactForm,
     CloudTag,
-  },
-  methods: {
-    encode(data) {
-      return Object.keys(data)
-        .map(
-          (key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
-        )
-        .join("&");
-    },
-    handleSubmit(event) {
-      event.preventDefault();
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: this.encode({
-          "form-name": "contact",
-          ...this.form,
-        }),
-      })
-        .then(() => {
-          this.isAlert = true;
-          this.typeAlert = "success";
-          this.alertText =
-            "Se ha enviado su consulta, nos pondremos en contacto con usted lo antes posible";
-        })
-        .catch((error) => {
-          this.typeAlert = "error";
-          this.isAlert = true;
-          this.alertText = "Ha ocurrido un error, intente mas tarde";
-          console.error(error);
-        });
-    },
+    JaguarAnimation
   },
 };
 </script>
@@ -55,7 +26,7 @@ export default {
 .form-view {
   min-height: 100vh;
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: minmax(300px, max-content) 1fr;
   place-items: center;
   @media screen and (max-width: 600px) {
     grid-template-columns: 1fr;
