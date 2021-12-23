76e59a4843a4dfeb9c86f11e1747fe377030a586
Jaime A Torrealba C <solucionesinformaticasjtc@gmail.com>
2021-12-22 21:41:53 -0300

    add controls to jaguar animation

--- src/components/contacme/JaguarAnimation.vue
+++ src/components/contacme/JaguarAnimation.vue
@@ -6,23 +6,38 @@
 <script>
 import { gsap } from "gsap";
 import { useMediaQuery } from "@vueuse/core";
-import { onMounted } from "vue";
 
 export default {
   setup() {
     const isLargeScreen = useMediaQuery("(min-width: 780px)");
     if (isLargeScreen) {
       setTimeout(() => {
-           let dragon = gsap.timeline({ repeat: -1 });
-          dragon.to(".position-jaguar", {
-            duration: 4,
-            x: "110vw",
-            y: 0,
-            ease: "sine.in",
-            delay: 2,
-          });
+        const jaguar = document.querySelector(".jaguarSprites");
+        let dragon = gsap.timeline({ repeat: -1 });
+        dragon.to(".position-jaguar", {
+          duration: 4,
+          x: "110vw",
+          y: 0,
+          ease: "sine.in",
+          delay: 5,
+        });
+        jaguar.addEventListener("mouseenter", function () {
+          dragon.pause();
+        });
+        jaguar.addEventListener("mouseleave", function () {
+          dragon.resume();
+          jaguar.style.webkitAnimationPlayState = "running";
+        });
+         jaguar.addEventListener("click", function () {
+           if(jaguar.style.webkitAnimationPlayState === "paused"){
+             jaguar.style.webkitAnimationPlayState = "running";
+           } else{
+             jaguar.style.webkitAnimationPlayState = "paused";
+           }
+        });
       }, 500);
     }
+
     return { isLargeScreen };
   },
   mounted() {},
@@ -38,6 +53,7 @@ export default {
   background-repeat: no-repeat;
   background-position: center center;
   animation: move 0.8s steps(12) infinite;
+  cursor: pointer;
 }
 @keyframes move {
   from {
